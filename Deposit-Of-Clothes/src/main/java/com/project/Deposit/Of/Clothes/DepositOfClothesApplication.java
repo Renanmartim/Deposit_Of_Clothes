b2227f3d55f5b5fcdf07ca734bf261939925293e
Renan Martim <117313515+Renanmartim@users.noreply.github.com>
2023-08-04 22:35:26 -0300

    Add files via upload

---
+++ Deposit-Of-Clothes/src/main/java/com/project/Deposit/Of/Clothes/DepositOfClothesApplication.java
@@ -0,0 +1,13 @@
+package com.project.Deposit.Of.Clothes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositOfClothesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositOfClothesApplication.class, args);
+	}
+
+}
